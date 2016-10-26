08aeeeb7b556dbad849aab1fa7cd28b718f24708
andrnors <andrnors@stud.ntnu.no>
2016-10-26 12:39:27 +1000

    bmi calcutions

--- YWWACP_Core/YWWACP.Core/ViewModels/FirstViewModel.cs
+++ YWWACP_Core/YWWACP.Core/ViewModels/FirstViewModel.cs
@@ -3,6 +3,7 @@ using MvvmCross.Core.ViewModels;
 using System.Windows.Input;
 using Android.App;
 using Android.Content;
+using OxyPlot;
 using YWWACP.Core.Interfaces;
 using YWWACP.Core.Models;
 using YWWACP.Core.ViewModels.Community;
@@ -41,6 +42,7 @@ namespace YWWACP.Core.ViewModels
             this.database = database;
             UserId = prefUserInfo.GetString("UserId", "");
             InitDb();
+            CalculateBmi();
    
             OpenHealthPlanCommand = new MvxCommand(() => ShowViewModel<HealthPlanViewModel>(new { userid = UserId }));
             OpenGraphCommand = new MvxCommand(() => ShowViewModel<GraphViewModel>(new { userid = UserId }));
@@ -99,8 +101,8 @@ namespace YWWACP.Core.ViewModels
             await database.InsertTableRow(new MyTable()
             {
                 Age = "Some old bitch",
-                Height = "Some heigh bitch",
-                Weight = "You Fat bitch",
+                Height = "184.0",
+                Weight = "77.0",
                 Name = "Some old nasty chick's name",
                 UserId = UserId
             });
@@ -200,10 +202,73 @@ namespace YWWACP.Core.ViewModels
                 UserId = UserId
 
             });
+            
+        }
 
-        
+        public void OnResume()
+        {
+            CalculateBmi();
+        }
 
+
+        private string bmi;
+        public string BMI
+        {
+            get { return bmi; }
+            set
+            {
+                SetProperty(ref bmi, value);
+                RaisePropertyChanged(() => BMI);
+            }
+        }
+
+        private string healthStatus;
+
+        public string HealthStatus { get { return healthStatus; } set { SetProperty(ref healthStatus, value); RaisePropertyChanged(() => HealthStatus); } }
+
+        private async void CalculateBmi()
+        {
+            var profiles = await database.GetTable();
+
+            foreach (var profile in profiles)
+            {
+                if (UserId == profile.UserId && profile.ThreadID == null && profile.CommentID == null &&
+                    profile.GoalId == null && profile.ExerciseId == null && profile.MealId == null)
+                {
+                    
+                    var whey = Convert.ToDouble(profile.Weight);
+                    var heightCM = Convert.ToDouble(profile.Height);
+                    var heightMeter = heightCM / 100;
+
+                    var bmi = (whey / (heightMeter * heightMeter));
+                    var b = bmi.ToString();
+                    b = b.Substring(0, b.IndexOf('.') + 2);
+                    BMI = b;
+                    if (bmi < 18.5)
+                    {
+                        HealthStatus = "Eat more!";
+                    }
+                    else if (bmi < 25)
+                    {
+                        HealthStatus = "Perfect body!";
+                    }
+                    else if (bmi > 25 && bmi < 30)
+                    {
+                        HealthStatus = "Consider Weightloss";
+                    }
+                    else
+                    {
+                        HealthStatus = "See a doctor";
+                    }
+                    break;
+                }
+                
+
+            }
 
         }
+
+
+
     }
 }
